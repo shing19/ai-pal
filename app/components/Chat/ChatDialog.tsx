6fd2aa43b8addcc4c7be69009d46d0745753203e
Shing <shing19@outlook.com>
2024-04-08 01:05:44 +0800

    feat: 修改已有的context message内容

--- app/components/Chat/ChatDialog.tsx
+++ app/components/Chat/ChatDialog.tsx
@@ -2,7 +2,7 @@
 import React, { useContext, useState } from 'react'
 import { Message } from 'ai/react'
 import { Button } from "@/components/ui/button"
-import { Cross2Icon } from '@radix-ui/react-icons'
+import { CheckIcon, Cross2Icon, Pencil1Icon } from '@radix-ui/react-icons'
 import { PalContext } from '../Context/PalContext'
 import { Badge } from "@/components/ui/badge"
 
@@ -11,14 +11,21 @@ interface ChatDialogProps {
     conversationCreatedAt: Date | undefined
 }
 
-interface ShowDeleteButtonsState {
+interface showButtonsState {
+    [key: string]: boolean;
+}
+
+interface editState {
     [key: string]: boolean;
 }
 
 
 const ChatDialog = ({ messages, conversationCreatedAt }: ChatDialogProps) => {
     const { contextMessages, projectConversations, updateConversationCreatedAt, updateContextMessages } = useContext(PalContext);
-    const [showDeleteButtons, setShowDeleteButtons] = useState<ShowDeleteButtonsState>({});
+    const [showButtons, setshowButtons] = useState<showButtonsState>({});
+    const [isEditing, setIsEditing] = useState(false);
+    const [edieState, setEditState] = useState<editState>({});
+    const [newContent, setNewContent] = useState('');
 
     // 拖拽事件
     const handleDragStart = (e: React.DragEvent<HTMLDivElement>, message: Message) => {
@@ -28,7 +35,7 @@ const ChatDialog = ({ messages, conversationCreatedAt }: ChatDialogProps) => {
 
     // 删除对话（仅作用在context里）
     const toggleDeleteButton = (messageId: string) => {
-        setShowDeleteButtons((prevState: ShowDeleteButtonsState) => ({
+        setshowButtons((prevState: showButtonsState) => ({
             ...prevState,
             [messageId]: !prevState[messageId],
         }));
@@ -56,6 +63,36 @@ const ChatDialog = ({ messages, conversationCreatedAt }: ChatDialogProps) => {
         }
     };
 
+
+    // 编辑信息
+    const handleEdit = (messageId: string) => {
+        toggleEditState(messageId)
+        setNewContent(messages.find(message => message.id === messageId)?.content || '')
+    };
+    const handleCancel = (messageId: string) => {
+        toggleEditState(messageId)
+    }
+    const toggleEditState = (messageId: string) => {
+        setEditState((prevState: editState) => ({
+            ...prevState,
+            [messageId]: !prevState[messageId],
+        }));
+    }
+    const handleSave = (messageId: string) => {
+        // 在 messages 里找到id相同的，然后更新content
+        const newMessages = messages.map(message => {
+            if (message.id === messageId) {
+                return {
+                    ...message,
+                    content: newContent
+                }
+            }
+            return message
+        })
+        updateContextMessages(newMessages)
+        toggleEditState(messageId)
+    }
+
     // 在display conversation的时候，不显示context message
     let displayMessages = messages;
     if (conversationCreatedAt !== undefined) {
@@ -71,7 +108,7 @@ const ChatDialog = ({ messages, conversationCreatedAt }: ChatDialogProps) => {
     return (
         <div>
             {displayMessages.length > 0 && displayMessages.map((message) => {
-                const showDeleteButton = showDeleteButtons[message.id] || false;
+                const showButton = showButtons[message.id] || false;
 
                 return (
                     <div
@@ -81,7 +118,7 @@ const ChatDialog = ({ messages, conversationCreatedAt }: ChatDialogProps) => {
                         onMouseLeave={() => toggleDeleteButton(message.id)}
                     >
                         <div
-                            className='flex justify-between'
+                            className='flex justify-between h-6'
                             draggable
                             onDragStart={(e) => handleDragStart(e, message)}
                         >
@@ -89,17 +126,45 @@ const ChatDialog = ({ messages, conversationCreatedAt }: ChatDialogProps) => {
                                 {message.role === 'assistant' ? <Badge>Bot</Badge> : <Badge variant="secondary">User</Badge>}
                                 {message.createdAt && <span className="text-sm text-muted-foreground">{new Date(message.createdAt).toLocaleString()}</span>}
                             </div>
-                            {showDeleteButton && !conversationCreatedAt && (
-                                <Button variant="destructive" size="icon" className="h-5 w-5" onClick={() => handleDelete(message.id)}>
-                                    <Cross2Icon className="h-3 w-3" />
-                                </Button>
+                            {showButton && !conversationCreatedAt && (
+                                <div className="space-x-1">
+                                    <Button variant="secondary" size="icon" className="h-5 w-5" onClick={() => handleEdit(message.id)}>
+                                        <Pencil1Icon className="h-3 w-3" />
+                                    </Button>
+                                    <Button variant="destructive" size="icon" className="h-5 w-5" onClick={() => handleDelete(message.id)}>
+                                        <Cross2Icon className="h-3 w-3" />
+                                    </Button>
+                                </div>
                             )}
                         </div>
                         <div className="ml-1 mt-2">
-                            {message.content &&
-                                message.content.split('\n').map((currentTextBlock: string, index: number) => {
-                                    return <p key={message.id + index}>{currentTextBlock}</p>;
-                                })}
+                            {edieState[message.id] ? (
+                                <div className='flex flex-col space-y-2 items-center'>
+                                    <textarea
+                                        value={newContent}
+                                        onChange={(e) => setNewContent(e.target.value)}
+                                        className="w-full h-auto border-none outline-none rounded"
+                                        style={{ resize: 'none' }}
+                                    >
+                                    </textarea>
+                                    <div className='flex space-x-2'>
+                                        <Button variant="outline" size="icon" className="h-5 w-5" onClick={() => handleSave(message.id)}>
+                                            <CheckIcon className="h-3 w-3" />
+                                        </Button>
+                                        <Button variant="outline" size="icon" className="h-5 w-5" onClick={() => handleCancel(message.id)}>
+                                            <Cross2Icon className="h-3 w-3" />
+                                        </Button>
+
+                                    </div>
+                                </div>
+                            ) : (
+                                <div>
+                                    {message.content &&
+                                        message.content.split('\n').map((currentTextBlock: string, index: number) => {
+                                            return <p key={message.id + index}>{currentTextBlock}</p>;
+                                        })}
+                                </div>
+                            )}
                         </div>
                     </div>
                 );
